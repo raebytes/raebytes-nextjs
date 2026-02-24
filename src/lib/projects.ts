b37c1c420f4b5bd35f4603ac50d5c4172fa75f9a
inkflow <tim.braun@inkflow.de>
2026-02-24 22:05:11 +0100

    refactor: remove unused project getter

--- src/lib/projects.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-const projectsDir = path.join(process.cwd(), "content/projects");
-
-export function getProject(slug: string) {
-    const filePath = path.join(projectsDir, `${slug}.mdx`);
-    return fs.readFileSync(filePath, "utf8");
-}
